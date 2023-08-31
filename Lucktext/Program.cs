d8ba5e4e6ec01a4290a10d23ea56834ef4d81893
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-08-31 10:26:53 +0200

    första committen

---
+++ Lucktext/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string val ="";
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff ja/ nej");
+val = Console.ReadLine();
+
+
+if (val == "ja")
+{
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("du dog");
+}
+else if (val == "nej")
+{
+    Console.WriteLine("jabfdkjabfknlabfklnbff");
+    Console.WriteLine("jabfdkjabfknlabfklnbff");
+    Console.WriteLine("jabfdkjabfknlabfklnbff");
+    Console.WriteLine("jabfdkjabfknlabfklnbff");
+    Console.WriteLine("jabfdkjabfknlabfklnbff");
+    Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff springa/smyga");
+}
+
+if (val == "springa")
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff");
+Console.WriteLine("jabfdkjabfknlabfklnbff du blev uptäck och dog");
+ else if (val == "smyga")
+ {
+ Console.WriteLine("jabfdkjabfknlabfklnbff");
+ Console.WriteLine("jabfdkjabfknlabfklnbff");
+ Console.WriteLine("jabfdkjabfknlabfklnbff");
+ Console.WriteLine("jabfdkjabfknlabfklnbff");
+ Console.WriteLine("jabfdkjabfknlabfklnbff du flydde");
+}
+
+ Console.WriteLine("tryck ENTER för att stänga av");
+ Console.ReadLine();
